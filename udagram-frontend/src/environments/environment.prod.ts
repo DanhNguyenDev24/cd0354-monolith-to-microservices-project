8601b5545c6a22a8d074539c681b5abaf9f90f48
Danh Nguyen <ncdanhvn@gmail.com>
2024-08-06 23:29:12 +0700

    feat: update the env vars

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8bc92aac24cc4f51835a2273b755909-185408932.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
